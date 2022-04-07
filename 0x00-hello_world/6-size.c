731cefcb0ffe83a875ff22ab04a88e42f7b40320
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-07 20:04:17 +0000

    cfdsdfdsfDdssDdsscxS

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,8 @@ printf("Size of char:%d bytes(s)\n", sizeof(char));
 printf("Size of int:%d bytes(s)\n", sizeof(int));
 printf("Size of long int:%ld bytes(s)\n", sizeof(long int));
 printf("Size of long long int:%lld bytes(s)\n", sizeof(long long int));
-printf("Size of float:%f bytes(s)", sizeof(float));
+printf("Size of float:%d bytes(s)", sizeof(float));
+
+return 0
 
 }
