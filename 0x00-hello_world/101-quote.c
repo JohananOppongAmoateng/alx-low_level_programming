5a3d15229294771bfbfaa666309d4fc77e353766
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-07 20:40:48 +0000

    cfdsdfdsfDdssDdsscsxSnns

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the phrase "with proper grammar,
+ *but the outcome is a piece of art,"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main()
+{
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return 1;
+} 
