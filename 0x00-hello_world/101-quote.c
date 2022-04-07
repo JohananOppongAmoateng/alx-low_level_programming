96c5bccb4197f67c5c8b059e470420afed07709a
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-07 20:43:01 +0000

    cfdsdfdsfDdssDdsscsxSnnss

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
-int main()
+int main(void)
 {
-fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 1;
-} 
+fprintf(stderr, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n");
+return (1);
+}
