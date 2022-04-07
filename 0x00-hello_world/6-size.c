9b854d3df4c330436813266f0fb9e2691002824a
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-07 20:11:11 +0000

    cfdsdfdsfDdssDdsscxSnn

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-printf("Size of char: %d bytes(s)\n", sizeof(char));
-printf("Size of int: %d bytes(s)\n", sizeof(int));
-printf("Size of long int: %ld bytes(s)\n", sizeof(long int));
-printf("Size of long long int: %lld bytes(s)\n", sizeof(long long int));
-printf("Size of float: %d bytes(s)", sizeof(float));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of long long int: %lld byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)", sizeof(float));
 
 return (0);
 
