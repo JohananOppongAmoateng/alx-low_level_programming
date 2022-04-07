b998c64b847acf518c407b0ce65b5d224bb76026
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-07 19:57:21 +0000

    cfdsdfdsfDdssDdssS

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+
 /**
- *fljsbjksnfljnsflnksds
- *nad aljsdnlsadsakdnk
-*/
+ * main - prints the phrase "with proper grammar,
+ *but the outcome is a piece of art,"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
-    printf("")
+printf("Size of char:%d bytes(s)", sizeof(char));
+printf("Size of int:%d bytes(s)", sizeof(int));
+printf("Size of long int:%d bytes(s)", sizeof(long int));
+printf("Size of long long int:%d bytes(s)", sizeof(long long int));
+printf("Size of float:%d bytes(s)", sizeof(float));
+
 }
