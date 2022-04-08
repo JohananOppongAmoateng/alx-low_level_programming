440f60fc4ca8c0461bbc1106f77fb737da7dd9da
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-08 16:14:59 +0000

    task 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints the phrase "with proper grammar,
+ *but the outcome is a piece of art,"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+int num;
+num = 0;
+while (num < 10)
+{
+putchar(num);
+num += 1;
+}
+putchar('\n');
+return (0);
+}
