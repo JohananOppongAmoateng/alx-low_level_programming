afdb09bd58a760bb6b8662370b94fbfac928902a
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-08 15:15:48 +0000

    task 1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+
+/**
+ * main - prints the phrase "with proper grammar,
+ *but the outcome is a piece of art,"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+int num;
+num = 97;
+while (num <= 122)
+{
+char c2 = (char)num;
+num += 1;
+putchar(c2);
+}
+}
