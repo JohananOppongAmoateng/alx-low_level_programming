d1fa41b392b2ac2ab6a15d39b6c3fed0cccd4d91
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-14 20:13:00 +0000

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+/**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
@@ -8,7 +13,7 @@
  * Return: largest number
  */
 
-int main (void)
+int main(void)
 {
 
 int largest_number(int a, int b, int c)
