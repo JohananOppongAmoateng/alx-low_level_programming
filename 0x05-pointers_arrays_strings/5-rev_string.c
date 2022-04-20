ec84687f3ceace957d2073610790554d64e9d81e
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-20 21:44:39 +0000

    task 8

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,28 @@
 #include "main.h"
 
-
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
 void rev_string(char *s)
 {
+	char tmp;
+	int i, len, len1;
 
-while (s[i] != '\0')
-{
-i++;
-}
+	len = 0;
+	len1 = 0;
 
-leng = i - 1;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-for (j = 0 ; leng >= j ; leng--)
-{
-_putchar(s[leng]);
-}
-_putchar('\n');
-}
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
