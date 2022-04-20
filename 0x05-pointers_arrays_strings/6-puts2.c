c344195ca1279b6ef13de83d08ae21ffedeeec58
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-20 19:39:50 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * puts2 - prints a string in reverse, followed by a new line
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+int i;
+int j;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+
+i = i - 1;
+
+for (j = 0 ; j <= i ; j++)
+{
+if (i % 2 == 1)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
