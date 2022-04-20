d906fac9b701471605f778b161e201295e6615fb
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-20 13:22:59 +0000

    Write a function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+
+#include "main.h"
+
+
+
+
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,8 @@ int _strlen(char *s);
 void swap_int(int *a, int *b);
 int _putchar(char c);
 void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+
 
