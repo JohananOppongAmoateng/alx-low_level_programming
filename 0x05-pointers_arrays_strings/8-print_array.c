2bb056a8498348c513143d8d856a77880a1bd2c7
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-20 21:26:21 +0000

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * print_array - prints half of a string
+ * followed by a new line
+ * @a: string to be printed
+ * @n: string to be printed
+ */
 void print_array(int *a, int n)
 {
 int i;
