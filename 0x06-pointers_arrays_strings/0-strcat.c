91802a5cf3892fb3039f17d92c8fc31495caf616
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-23 21:11:30 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * _strcat - Short description, single line
+ * @dest: Description of parameter x
+(* @src:a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+
+j = 0;
+i = 0;
+while (dest[i] != 0)
+{
+i++;
+}
+
+while (src[j] != 0)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+return (*dest);
+}
