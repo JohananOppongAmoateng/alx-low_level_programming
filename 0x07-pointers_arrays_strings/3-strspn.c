53a6128052e144f9ef06dd442e38793df0878dc5
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-26 21:01:23 +0000

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 
 
+
+/**
+ * _strspn - prints buffer in hexa
+ * @s: the address of memory to print
+ * @accept: the size of the memory to print
+ * Return: s.
+ */
+
+
 unsigned int _strspn(char *s, char *accept)
 {
-	int i,j,k,man;
+	int i, j, k, man;
+
 	k = 0;
 	for (i = 0; s[i] != '\0' ; i++)
 	{
 		man = 0;
-		for (j = 0; accept[j] != '\0';j++)
+		for (j = 0 ; accept[j] != '\0' ; j++)
 		{
 			if (accept[j] == s[i])
 			{
