00336dd26c197cad1bfe719fd33100209f211558
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-04-27 01:03:19 +0000

    task 4

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
