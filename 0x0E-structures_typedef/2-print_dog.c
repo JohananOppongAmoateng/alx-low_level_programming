8b3567eca13a3eb5f232bd1d64b09eb009645017
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-05-11 20:11:57 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,9 +8,31 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s", d->name);
+	if (d == NULL)
+	{
+		printf("");
+	}
+	else
+	{
+		if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else
+	{
+		printf("Name: %s", d->name);
+	}
 	printf("\n");
+
 	printf("Age: %f", d->age);
 	printf("\n");
-	printf("Owner: %s", d->owner);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	else
+	{
+		printf("Owner: %s", d->owner);
+	}
+	}
 }
