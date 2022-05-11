1bad645f4d084ada4bdad471fee4a49cd1fc3aea
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-05-11 13:39:04 +0000

    task 0 and 1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+ 
+int main(void)
+{
+	printf("%s",__FILE__);
+	return (0);
+}
