8fc7a5576343c5ae05c5e109eafef31bfb78c912
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-05-13 22:02:44 +0000

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,31 @@
-int int_index(int *array, int size, int (*cmp)(int));
+/**
+ * int_index - check if the absolute value of a number is 98
+ *@array: the integer to check
+ *@size: the integer to check4
+ *@cmp: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, j;
+
+	j = 0;
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	if (j > 0)
+	{
+		return (0);
+	}
+	return (-1);
+}
