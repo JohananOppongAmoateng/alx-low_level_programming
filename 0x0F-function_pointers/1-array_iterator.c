4ce249d90f78789a47ac2ac479719c4c7e9c4ff8
Johanan OPpong Amoateng <johananoppongamoateng2001@gmail.com>
2022-05-13 21:31:59 +0000

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,21 @@
-void array_iterator(int *array, int size, void (*action)(int));
+#include <stddef.h>
+
+
+/**
+ * array_iterator - prints an integer, in hexadecimal
+ * @array: the integer to print
+ * @size: the integer to print
+ * @action: the integer to print
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
