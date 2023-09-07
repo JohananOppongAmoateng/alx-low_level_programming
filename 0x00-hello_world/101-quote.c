e84a8f2ca86b59019a1b740163598ce1dcd6018c
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-07 19:55:36 +0000

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
