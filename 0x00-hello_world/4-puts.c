5c6bb0fa90cc2d6753ce389cd6d431f352f11011
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-07 17:32:15 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Main Fuction
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
