5e85d60f38f10a95cb3794e36bfc716a3b57e5c7
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-09 18:45:49 +0000

    tasks

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-main
+ *
+ *Return:0 if success
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10 ; a++)
+	{
+		printf("%d"o, a);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main-main
+ *
+ *Return:0
+*/
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar("%d", a);
+		a++
+	}
+	return (0);
+}
