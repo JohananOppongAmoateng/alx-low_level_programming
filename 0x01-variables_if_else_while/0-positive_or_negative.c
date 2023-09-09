629a4d0524fa7fe5ebe441a505aa089ea92f0462
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-09 18:56:08 +0000

    tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is greater than zero", n);
-	}
 	else if (n < 0)
 	{
 		printf("%d is less than zero", n);
-	else {
+	}
+	else
+	{
 		printf("%d is zero", n);
 	}
 	return (0);
