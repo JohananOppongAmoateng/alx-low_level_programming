ca027d443f53c64a634f98dce544f8d76c3e4bdc
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-19 00:00:23 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen- mams
+* @s:s
+* Return: 0
+*/
+
+int _strlen(char *s)
+{
+	int i;
+	i = 0
+	while (s[i] != "\0")
+	{
+		i++;
+	}
+	return (i);
+}
