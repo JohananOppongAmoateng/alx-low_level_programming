6bf15bcfff475fa6152974c7a990db010af61f16
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-19 00:40:23 +0000

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts-ad
+* @str:asd
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar("\n");
+}
