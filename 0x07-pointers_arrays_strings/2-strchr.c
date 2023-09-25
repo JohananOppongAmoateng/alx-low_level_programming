3948e4f0cfa672b77210b7c16ba682b3b2a74472
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-25 22:06:59 +0000

    Asda

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include  <stdio.h>
+#include "main.h"
 
 /**
 * _strchr-sad
@@ -14,7 +14,7 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			return (s[i]);
+			return (&s[i]);
 		}
 	}
 	return (NULL);
