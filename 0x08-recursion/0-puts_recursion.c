13c8bed2502e35aff85a52c4936f7ef0660032f4
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-27 20:30:44 +0000

    ghvj

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion-asda
+* @s: nsdfn
+*/
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i]  == '\0')
+	{
+		return (NULL);
+	}
+	_putchar("%s",s[i]);
+	return _puts_recursion(s[i++]);
+}
