1268c7c11542770df9d119b608bf7073b925bd11
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-27 20:39:22 +0000

    ghvj

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,10 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i]  == '\0')
+	if (s[i]  != '\0')
 	{
-		return;
+ 		_putchar("%s",s[i]);
+		i++;
+	 	_puts_recursion(s[i]);
 	}
-	_putchar("%s",s[i]);
-	i++;
-	return _puts_recursion(s[i]);
 }
