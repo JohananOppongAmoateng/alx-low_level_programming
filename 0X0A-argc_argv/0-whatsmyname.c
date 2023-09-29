145cafe50ae2f7a2fa1e6971879c4a4514c59e22
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-09-29 23:19:08 +0000

    asda

---
+++ 0X0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main-main
+* @argc: assad
+* @argv:sadsa
+* Return:ad
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		return (1);
+	}
+	printf("%s\n", argv[0]);
+	return (0);
+}
