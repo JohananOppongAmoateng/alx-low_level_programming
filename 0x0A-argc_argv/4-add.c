972b4e5e71f6ab4c850e1c2167265093e7ef60d3
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-10-01 22:29:46 +0000

    sdadsad

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,26 +9,29 @@
 * Return:(0);
 */
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	int i;
 	int result = 0;
 
-	for (i = 0; i < argc; i++)
+	if (argc == 1)
+		{
+			printf("%d\n", 0);
+		}
+	else
 	{
-		char *endptr;
-		strtol(argv[i], &endptr, 10);
+		for (i = 0; i < argc; i++)
+		{
+			char *endptr;
+			strtol(argv[i], &endptr, 10);
 
-		if (argc == 1)
-		{
-			printf("0");
+			if (*endptr != '\0' && *endptr != '\n')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			result = result + atoi(argv[i]);
 		}
-		else if (*endptr != '\0' && *endptr != '\n')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		result = result + atoi(argv[i]);
 	}
-	return (result);
+	printf("%d", result);
+	return (0);
 }
