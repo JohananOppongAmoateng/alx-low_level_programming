de3684799583025b35992d2fab924d8ffdfb330b
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-10-02 23:43:10 +0000

    tasks

--- 0x0B-malloc_free/3-alloc_graid.c
+++ 0x0B-malloc_free/3-alloc_graid.c
@@ -11,4 +11,36 @@
 
 int **alloc_grid(int width, int height)
 {
+	int i;
+	int j;
+
+	if (width <=  0 || height < = 0)
+	{
+		return (NULL);
+	}
+
+	int **grid = malloc(height *  sizeof(int * ));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j=0, j < i; j++)
+			{
+				free(grid[j];
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid)
 }
