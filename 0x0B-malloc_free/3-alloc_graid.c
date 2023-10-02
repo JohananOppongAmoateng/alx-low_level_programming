2d2eef22358d1c389a4010b2bbee577d3adfa073
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-10-02 22:50:27 +0000

    tasks

---
+++ 0x0B-malloc_free/3-alloc_graid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* aloc_grid - adsa
+* @width:dfsf
+* @height:dsfs
+* Return:Adas
+*/
+
+int **alloc_grid(int width, int height)
+{
+}
