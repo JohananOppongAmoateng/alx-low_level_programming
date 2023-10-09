7b96ad1c99e44cb78d182059daf4fe24491a81c7
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-10-09 23:15:54 +0000

    dfsfdsfsd

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
-* init_dog-ansa
-* @d:sadas
-* @name:sada
-* @age:asdsa
-* @owner:dasd
+* init_dog - inits a variable of
+*type dog
+*@d: dog identification
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+if (d != NULL)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,19 +1,29 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
-* print_dog - dfgchbj
-* @d:sadsa
+* print_dog - prints a struct dog
+*@d: dog identification
+*
 */
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
-		printf("");
-	}
+if (d == NULL )
+return; 
 
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Breed: %s\n", (d->breed != NULL) ? d->breed : "(nil)");
-	printf("Age: %d\n", d->age);
+if ((*d).name == NULL)
+printf("nil\n");
+else
+printf("Name: %s\n", d->name);
+
+if ((*d).age < 0)
+printf("nil\n");
+else
+printf("Age: %f\n", d->age);
+
+if ((*d).owner == NULL)
+printf("nil\n");
+else
+printf("Owner: %s\n", d->owner);
+
 }
